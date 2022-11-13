ed844d9791878808868dc928d3ef77ab8cc04620
Maciej Tabiś <maciek.tabis@gmail.com>
2022-11-13 18:50:03 +0100

    Add class active to the cliked link

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
-const titleClickHandler = function(){
-    console.log('Link was clicked!');
+const titleClickHandler = function(event){
+  const clickedElement = this;  
+  console.log('Link was clicked!');
   }
-  /* remove class 'active' from all article links  */
+  /*[DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
-
+  /* [DONE] add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
   /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll(".posts article.active");
+const activeArticles = document.querySelectorAll('.posts article.active');
   
   for(let activeArticle of activeArticles) {
     activeArticle.classList.remove('active')
@@ -22,8 +24,8 @@ const titleClickHandler = function(){
   /* find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
-  const links = document.querySelectorAll('.titles a');
+const links = document.querySelectorAll('.titles a');
   
-  for(let link of links){
+for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
